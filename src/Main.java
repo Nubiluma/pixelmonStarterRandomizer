20db11dc66729ba03c840c01989a6fb053451c03
Nubiluma <71071950+Nubiluma@users.noreply.github.com>
2020-09-18 14:19:18 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+private static Randomizer r = new Randomizer();
+
+    public static void main(String[] args) {
+        r.randomizeStarters();
+    }
+
+
+}
---
+++ src/Randomizer.java
@@ -0,0 +1,50 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class Randomizer {
+
+    private final Random random = new Random();
+    private List<String> possibleStarters;
+    
+    private void readStartersFile(){
+
+        try {
+            possibleStarters = Files.readAllLines(Paths.get("src/starters.txt"));
+
+        } catch (IOException e) {
+            System.err.println("File not found");
+            e.printStackTrace();
+        }
+        if (possibleStarters != null) {
+            System.out.println("Amount of possible starters: " + possibleStarters.size());
+        }
+    }
+
+    public void randomizeStarters(){
+
+        readStartersFile();
+
+        List<String> starters = new ArrayList<>();
+
+        for (int i = 0; i < 21; i++){
+
+            String randomStarter = possibleStarters.get(random.nextInt(possibleStarters.size()));
+
+            if (!starters.contains(randomStarter)){
+                starters.add(i, randomStarter);
+            }else {
+                i--;
+            }
+        }
+        System.out.println("Starters:");
+        for (String name : starters) {
+            System.out.println(name);
+        }
+
+    }
+}
